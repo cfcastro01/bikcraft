b3a0b47524fd0c6ab083c0b4565ceb2645a9fe39
cfcastro01 <cfcastro.rj@gmail.com>
2024-03-07 15:01:23 -0300

    formulario javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Ativar links
 
-const links = document.querySelectorAll('.header-menu a')
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-   const url = location.href;
-   const href = link.href;
+  const url = location.href;
+  const href = link.href;
 
-   if (url.includes(href)) {
-    link.classList.add("ativo")
-   }
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLink);
@@ -18,11 +18,10 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-   const elemento = document.getElementById(parametro);
-   if (elemento) {
-      elemento.checked = true;
-   }  
-
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
 parametros.forEach(ativarProduto);
@@ -32,23 +31,22 @@ parametros.forEach(ativarProduto);
 const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
-   const pergunta = event.currentTarget;
-   const controls = pergunta.getAttribute("aria-controls");
-   const resposta = document.getElementById(controls);
-   
-   resposta.classList.toggle("ativa");
-   const ativa = resposta.classList.contains("ativa");
-   pergunta.setAttribute("aria-expanded", ativa);
-   
-   console.log(ativa);
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+
+  console.log(ativa);
 }
 
 function eventosPerguntas(pergunta) {
-   pergunta.addEventListener('click', ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
-
 
 // Galeria de Fotos
 
@@ -56,22 +54,58 @@ const galeria = document.querySelectorAll(".bicicleta-imagens img");
 const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
 function trocarImagem(event) {
-   const img = event.currentTarget;
-   const media = matchMedia("(min-width: 1000px)").matches;
-   if (media) {
-      galeriaContainer.prepend(img); 
-   }
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
 }
 
 function eventosGaleria(img) {
-   img.addEventListener("click", trocarImagem);
+  img.addEventListener("click", trocarImagem);
 }
 
 galeria.forEach(eventosGaleria);
 
-
 // Animação
 
 if (window.SimpleAnime) {
-   new SimpleAnime();
+  new SimpleAnime();
 }
+
+// Formulário
+
+const formulario = document.querySelector("form");
+
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317A00;'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24 horas.</p>";
+  } else {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #E00000;'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraft.com</p>";
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  botao.innerText = "Enviando...";
+
+  const campos = formulario.querySelectorAll("[name]");
+  const data = new FormData();
+
+  campos.forEach((campo) => {
+    const name = campo.getAttribute("name");
+    const value = campo.value;
+    data.append(name, value);
+  });
+
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+
+formulario.addEventListener("submit", enviarFormulario);
